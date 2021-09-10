5dd326da08d06a29f9cba85f033ab40efac66610
Joo-SangWon <68213901+Joo-SangWon@users.noreply.github.com>
2021-09-10 17:22:41 +0900

    Add files via upload
    change TR continuous check problem
    KHopenAPI gives me just 600 rows at a time but solved.

--- TR.py
+++ TR.py
@@ -1,42 +1,9 @@
+import pandas as pd
 from pykiwoom.kiwoom import *
 import time
-import pandas as pd
 
 kiwoom = Kiwoom()
 kiwoom.CommConnect(block=True)
-
-# 싱글데이터 TR
-# df = kiwoom.block_request("opt10001",
-#                           종목코드 = "005930",
-#                           output="주식기본정보",
-#                           next=0) # next=0 단일정보조회시
-# print(df)
-
-# 멀티데이터 TR
-# 주식일봉차트조회
-# TR요청 (연속조회)
-# dfs = []
-# df = kiwoom.block_request("opt10081",
-#                           종목코드 = "005930",
-#                           기준일자 = "20210907",
-#                           수정주가구분 = 1,
-#                           output="주식일봉차트조회",
-#                           next=0)
-# print(df.head())
-# dfs.append(df)
-#
-# while kiwoom.tr_remained:
-#     df = kiwoom.block_request("opt10081",
-#                               종목코드 = "005930",
-#                               기준일자 = "20210907",
-#                               수정주가구분 = 1,
-#                               output = "주식일봉차트조회",
-#                               next=2)
-#     dfs.append(df)
-#     time.sleep(1)
-#
-# df= pd.concat(dfs)
-# df.to_excel("005930.xlsx")
 
 # 전종목 일봉데이터를 엑셀로 저장하기
 # 전종목 종목코드
@@ -47,11 +14,14 @@ codes = kospi + kosdaq
 # 문장열로 오늘 날짜 얻기
 now = datetime.datetime.now()
 today = now.strftime('%Y%m%d')
-
+num = 1
 # 전 종목의 일봉 데이터
 # enumerate 몇 번째 반복문인지 확인하고 싶을때 튜플형태로 반환
 # 그래서 i로 몇 번째 인지 알 수 있음(index, list의 element) 형태
+# TR 요청 안했었음 한 번에 600개 행만 보내줌
+# 장시작까지는 8700개의 행이 필요함
 for i, code, in enumerate(codes):
+    dfs=[]
     print(f"{i}/{len(codes)} {code}")
     df = kiwoom.block_request("opt10081",
                               종목코드=code,
@@ -59,6 +29,26 @@ for i, code, in enumerate(codes):
                               수정주가구분=1,
                               output="주식일봉차트조회",
                               next=0)
+    dfs.append(df)
+
+    while kiwoom.tr_remained:
+        print("{}th 종목의 Data가 남아있어 추가 TR 요청을 {} 번째 중입니다.".format(i, num))
+        df = kiwoom.block_request("opt10081",
+                                  종목코드=code,
+                                  기준일자=today,
+                                  수정주가구분=1,
+                                  output="주식일봉차트조회",
+                                  next=2)
+        dfs.append(df)
+        num += 1
+        time.sleep(1.5)
+
+    print("TR 추가 요청이 끝났습니다.")
+    df = pd.concat(dfs)
+    num = 1
     out_name = f"{code}.xlsx"
+
     df.to_excel(out_name)
     time.sleep(3.6)
+
+print("ALL TR finished")
